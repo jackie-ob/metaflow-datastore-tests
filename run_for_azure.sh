a898add8379a6b2d244d40750952c6d76bcaf535
Ubuntu <azureuser@hotdog.j3xuniusd2kuxpk1paxjvdq3ib.phxx.internal.cloudapp.net>
2022-06-25 00:00:11 +0000

    improvements

--- run_for_azure.sh
+++ run_for_azure.sh
@@ -1,10 +1,12 @@
-export METAFLOW_DATASTORE_SYSROOT_AZURE=test-azure-storage/datastore-bench
+export METAFLOW_DATASTORE_SYSROOT_AZURE=test-azure-storage/datastore-bench/$(date +%s)
 export METAFLOW_AZURE_STORAGE_ACCOUNT_URL=https://obbenchmark1.blob.core.windows.net/
 export METAFLOW_ARTIFACT_LOCALROOT=/mnt/metaflow-tmp
 export METAFLOW_CLIENT_CACHE_PATH=/mnt/metaflow_client
 export METAFLOW_DATASTORE_SYSROOT_LOCAL=/mnt/.metaflow
 
-export METAFLOW_AZURE_STORAGE_ACCESS_KEY=blah
+# export METAFLOW_AZURE_STORAGE_ACCESS_KEY=
 
+rm -rf $METAFLOW_ARTIFACT_LOCALROOT $METAFLOW_CLIENT_CACHE_PATH $METAFLOW_DATASTORE_SYSROOT_LOCAL
 mkdir -p $METAFLOW_ARTIFACT_LOCALROOT
-time ./run_tests.sh ../metaflow noop > azure_datastore_results.csv 2>azure_datastore_results.err
+
+time ./run_tests.sh ../metaflow $1 > azure_datastore_results.csv 2>azure_datastore_results.err
